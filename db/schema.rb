6a0830116ecfbcca21a72ea6a0ed5129def89930
hide1120 <54133489+hide1120@users.noreply.github.com>
2020-05-19 16:17:07 +0900

    更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_122255) do
+ActiveRecord::Schema.define(version: 2020_05_18_131338) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
